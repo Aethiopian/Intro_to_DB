dacf0b0b83f1382a99688d5ea195d9235834ccc2
Dambi <imail.oromo@gmail.com>
2024-10-22 06:20:50 +0300

    changes

--- task_2.sql
+++ task_2.sql
@@ -26,8 +26,8 @@ CREATE TABLE Books (
 -- Create customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     phone_number VARCHAR(15)
     address TEXT
 );
